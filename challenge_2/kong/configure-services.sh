00930597e8c82136a78e522c11ac70cbcb25e812
alanfoster <alan@alanfoster.me>
2021-07-31 18:10:41 +0100

    use consistent naming

--- challenge_2/kong/configure-services.sh
+++ challenge_2/kong/configure-services.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 curl -v -i -X POST http://kong:8001/services --data name=my_service --data url='http://webservice:8000'
 curl -v -i -X POST http://kong:8001/services/my_service/routes --data 'paths[]=/' --data name=my_service_main
 
-# TODO: Use a more secure ssh password for the website, currently it's root:admin
-# curl -u root:admin http://website/health 2>/dev/null
+# TODO: Use a more secure ssh password for the webservice, currently it's root:admin
+# curl -u root:admin http://webservice/health 2>/dev/null
